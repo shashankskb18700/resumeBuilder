909c79287c920dbe11bd26c49fe104493798bd33
shashank <shashankskb18700@gmail.com>
2022-02-26 01:07:20 +0530

    inital commit

---
+++ Client/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>i am App</div>;
+};
+
+export default App;
---
+++ Client/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+
+import App from "./App.js";
+import React from "react";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
