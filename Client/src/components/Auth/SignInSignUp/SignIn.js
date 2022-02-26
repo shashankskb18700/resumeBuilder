96d2f99b099eabd03d0553d58281605910f13bee
shashank <shashankskb18700@gmail.com>
2022-02-27 01:02:05 +0530

    added env

--- Client/src/components/Auth/SignInSignUp/SignIn.js
+++ Client/src/components/Auth/SignInSignUp/SignIn.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+
   return (
     <div>
       <form>
-        <input type="name" name="name" placeholder="entre your email" />
+        <input
+          type="name"
+          name="name"
+          placeholder="entre your email"
+          // onChange={onChange}
+        />
         <input
           type="password"
           name="password"
+          // onChange={onChange}
           placeholder="entre your password"
         />
         <button>Sign In</button>
--- Client/src/firebase/fbase.js
+++ Client/src/firebase/fbase.js
@@ -6,12 +6,12 @@ import * as auth from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDcRVWKFBP1bpfs7wPL1z178oYEUDYBfU",
-  authDomain: "redeemer-1a3d9.firebaseapp.com",
-  projectId: "redeemer-1a3d9",
-  storageBucket: "redeemer-1a3d9.appspot.com",
-  messagingSenderId: "4353583596",
-  appId: "1:4353583596:web:c639101bc179b323e178c9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
